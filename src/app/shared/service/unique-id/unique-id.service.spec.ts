5cb3be714744bdc8027a157b0dcb6e39e336e634
mateus.silva <mateus.silva@iatec.com>
2022-11-04 09:26:03 -0300

    Improvement tests

--- src/app/shared/service/unique-id/unique-id.service.spec.ts
+++ src/app/shared/service/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,36 @@
 import { UniqueIdService } from "./unique-id.service";
 
+let service: UniqueIdService = new UniqueIdService();
+
+beforeEach(() => {
+    service = new UniqueIdService();
+});
+
 describe(UniqueIdService.name, () => {
     it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-        const service = new UniqueIdService();
         const id = service.generatedUniqueIdWithPrefix('app');
-        expect(id).toContain('app-');
+        expect(id.startsWith('app-')).toBeTrue();
+    });
+
+    it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
+        // const firstId = service.generatedUniqueIdWithPrefix('app-');
+        // const secondId = service.generatedUniqueIdWithPrefix('app-');
+
+        // expect(firstId).not.toBe(secondId);
+
+        const ids = new Set();
+
+        for(let i = 0; i < 50; i++){
+            ids.add(service.generatedUniqueIdWithPrefix('app-'));
+        }
+
+        expect(ids.size).toBe(50);
+    });
+
+    it(`#numberOfGeneratedUniqueId should return the number of generatedIds when called`, () => {
+        service.generatedUniqueIdWithPrefix('app-');
+        service.generatedUniqueIdWithPrefix('app-');
+
+        expect(service.numberOfGeneratedUniqueId).toBe(2);
     });
 });
