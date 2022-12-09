e8468d4c12db3cbad2a0dea58156c684958a5f1c
mateus.silva <mateus.silva@iatec.com>
2022-12-09 08:18:23 -0300

    Add photo frame component

---
+++ src/app/shared/components/photo-frame/component/photo-frame.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-photo-frame',
+    templateUrl: './photo-frame.component.html',
+    styleUrls: ['./photo-frame.component.scss']
+})
+export class PhotoFrameComponent{}
