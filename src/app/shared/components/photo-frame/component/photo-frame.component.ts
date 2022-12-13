3eefb2ec3e135bb956ad78e4d859c93e2caedca5
mateus.silva <mateus.silva@iatec.com>
2022-12-13 13:54:24 -0300

    Improvement photo-frame

--- src/app/shared/components/photo-frame/component/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/component/photo-frame.component.ts
@@ -1,8 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
     selector: 'app-photo-frame',
     templateUrl: './photo-frame.component.html',
     styleUrls: ['./photo-frame.component.scss']
 })
-export class PhotoFrameComponent{}
+export class PhotoFrameComponent{
+    @Output() liked: EventEmitter<void> = new EventEmitter<void>();
+    @Input() description: string = '';
+    @Input() src: string = '';
+    @Input() likes: number = 0;
+}
