b67da1f52a211a29de1fe99e7617b42585623cea
mateus.silva <mateus.silva@iatec.com>
2023-01-08 17:39:52 -0300

    Improvement tests

--- src/app/shared/components/photo-frame/component/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/component/photo-frame.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testing";
 import { PhotoFrameModule } from "../photo-frame.module";
 import { PhotoFrameComponent } from "./photo-frame.component";
 
@@ -17,5 +17,42 @@ describe(PhotoFrameComponent.name, () => {
 
     it('Should create component', () => {
         expect(component).toBeTruthy();
-    })
-});
+    });
+
+    it(`#${PhotoFrameComponent.prototype.like.name} should trigger (@Output liked) 
+        once when called multiple times within debounce time`, fakeAsync(() => {
+            fixture.detectChanges();
+
+            let times = 0;
+
+            component.liked.subscribe(() => {
+                times++;
+            });
+
+            component.like();
+            component.like();
+            
+            tick(500);
+
+            expect(times).toBe(1);
+    }));
+
+    it(`#${PhotoFrameComponent.prototype.like.name} should trigger (@Output liked) two times
+        when called mulitple times within debounce time`, fakeAsync(() => {
+            fixture.detectChanges();
+
+            let times = 0;
+
+            component.liked.subscribe(() => {
+                times++;
+            });
+
+            component.like();
+            tick(500);
+
+            component.like();
+            tick(500);
+
+            expect(times).toBe(2);
+        }));
+}); 
